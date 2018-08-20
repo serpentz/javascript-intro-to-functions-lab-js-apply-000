e812fb6190c79d39fda7028ee33e850bd8033cd3
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-20 17:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
 
+
+  if(string === "I love you, Grandma."){
+    return "YES INDEED!"
+  }
   if(string.toLowerCase() === string){
     return "I can't hear you!"
   }
   if(string.toUpperCase() === string){
     return "YES INDEED!"
   }
-  if(string === "I love you, Grandma."){
-    return "YES INDEED!"
-  }
+
+
 }
