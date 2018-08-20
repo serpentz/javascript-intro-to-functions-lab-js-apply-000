b26d2e6d08990340007b6cebe161ffbc4e0e40e1
Absalom Cherinet <absalomcherinet@gmail.com>
2018-08-20 17:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ return string.toLowerCase()
 function logShout(string){
  console.log(string.toUpperCase())
 }
-function logShout(string){
+function logWhisper(string){
  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
